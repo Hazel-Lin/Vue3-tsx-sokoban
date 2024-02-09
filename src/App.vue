3acabb06ae3a3719ccf989776204ca1f783c8d0d
linhuizi <529246402@qq.com>
2024-02-09 17:53:42 +0800

    feat: update styles

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
-  <main font-sans p="x-4 y-10" text="center gray-700 dark:gray-200">
+  <main
+    font-sans class="absolute  top-50% left-50% translate--50%"
+    text="center gray-700 dark:gray-200"
+  >
     <RouterView />
   </main>
 </template>
