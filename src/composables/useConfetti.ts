517fad879e6904ae7ab2020403c388beb2dccd08
linhuizi <529246402@qq.com>
2024-02-14 00:03:22 +0800

    fix: update fireworks duration

--- src/composables/useConfetti.ts
+++ src/composables/useConfetti.ts
@@ -1,26 +1,24 @@
 import confetti from 'canvas-confetti'
 
-const duration = 5 * 1000
+const duration = 8 * 1000
 const end = Date.now() + duration
 
 function fireworksEffect() {
-  return (function frame() {
-    confetti({
-      particleCount: 7,
-      angle: 60,
-      spread: 55,
-      origin: { x: 0 },
-    })
-    confetti({
-      particleCount: 7,
-      angle: 120,
-      spread: 55,
-      origin: { x: 1 },
-    })
+  confetti({
+    particleCount: 7,
+    angle: 60,
+    spread: 55,
+    origin: { x: 0 },
+  })
+  confetti({
+    particleCount: 7,
+    angle: 120,
+    spread: 55,
+    origin: { x: 1 },
+  })
 
-    if (Date.now() < end)
-      requestAnimationFrame(frame)
-  })()
+  if (Date.now() < end)
+    requestAnimationFrame(fireworksEffect)
 }
 
 export function useConfetti() {
